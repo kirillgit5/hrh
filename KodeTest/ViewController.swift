15336b587eb9029f4ba7fb53391532a0de0c87d3
kramarkirill <kkramar@kantiana.ru>
2020-10-15 21:16:06 +0200

    Initial Commit

---
+++ KodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KodeTest
+//
+//  Created by Кирилл Крамар on 15.10.2020.
+//  Copyright © 2020 Кирилл Крамар. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
